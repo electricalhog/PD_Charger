bca32fa42ea85f124564c7efea905f20380974dc
electrialhog <electricalhog10@gmail.com>
2026-03-11 12:01:34 -0400

    remove unnecessary projects

--- Firmware_G474/Src/board/Board.cpp
+++
@@ -1,3 +0,0 @@
-#include "board/Board.h"
-
-// Intentionally empty: configuration is header-only for now.
--- STM32CubeIDE/Source_Test/Debug/Drivers/BSP/Components/tcpp0203/tcpp0203_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/tcpp0203/tcpp0203_reg.o: \
- ../Drivers/BSP/Components/tcpp0203/tcpp0203_reg.c \
- ../Drivers/BSP/Components/tcpp0203/tcpp0203_reg.h
-../Drivers/BSP/Components/tcpp0203/tcpp0203_reg.h:
